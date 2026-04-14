34a262ce3a38bb3e1c0158452624ffb0217c4933
Kriskr00s <valtinkristofer@gmail.com>
2026-04-14 14:52:11 +0300

    Create ForeignKey.sql

---
+++ ForeignKey.sql
@@ -0,0 +1,24 @@
+CREATE TABLE opilaneTunnis(
+opilaneTunnisId int Primary Key identity(1,1),
+kuupäev date not Null,
+opilaneId int, 
+Foreign Key (opilaneId) References opilane(opilaneId), --tabel(PK veerg)
+oppeaine varchar(25),
+hinne int)
+Select * from opilane;
+Select * from opilaneTunnis;
+
+--lisame andmeid opilanetunnis tabelisse
+INSERT INTO opilaneTunnis
+VALUES ('2025-04-14', 2, 'keemia', 4);
+
+--testTabel
+Create table testTabel(
+id int primary key);
+--tabeli kustutamine
+DROP Table testTabel;
+DROP Database Mihhailenko;
+
+--tabelirida kustutamine
+select * from opilane
+DELETE FROM opilane WHERE opilaneId=4;
