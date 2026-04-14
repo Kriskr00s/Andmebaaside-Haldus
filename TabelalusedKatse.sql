fda6a178960213442a8d322f07354eab575340d1
Kriskr00s <valtinkristofer@gmail.com>
2026-04-14 15:37:17 +0300

    Create TabelalusedKatse.sql

---
+++ TabelalusedKatse.sql
@@ -0,0 +1,22 @@
+--TEST KOHT
+
+CREATE TABLE opetaja(
+opetajaId int Primary Key identity(1,1),
+nimi varchar(25),
+ruum varchar(25),)
+
+INSERT INTO opetaja
+VAlUES ('Nimi', 213);
+
+CREATE TABLE opetamine(
+opetamineId int Primary Key identity(1,1),
+opetajaID int,
+Foreign Key (opetajaID) References opetaja(opetajaID),
+oppeaine varchar(25),
+maht int)
+
+INSERT INTO opetamine
+VALUES ( 1, 'keemia', 15);
+
+Select * from opetaja;
+Select * from opetamine;
